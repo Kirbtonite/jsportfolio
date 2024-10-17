9c6eb4d1b1546c7eab3310c4981c4569fe0ed6b3
Kirbtonite <southiphonhjacky@gmail.com>
2024-10-16 21:35:20 -0400

    Integration html and css for page project portfolio website done

--- src/scripts/components/Gsap.js
+++ src/scripts/components/Gsap.js
@@ -9,28 +9,13 @@ gsap.registerPlugin(Flip, ScrollTrigger, Draggable, TextPlugin);
 export default class Gsap {
   constructor() {
     this.init();
-    /*  this.checkWindowSize = setInterval(this.responsiveAnimated, 1000); */
   }
   init() {
     this.animationScrollTrigger();
     this.draggableElement();
     this.changeText();
   }
-  animationScrollTrigger() {
-    /*     gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: '.left',
-          scrub: true,
-        },
-      })
-      .to('.left', {
-        x: -100,
-      })
-      .to('.left', {
-        x: 200,
-      }); */
-  }
+  animationScrollTrigger() {}
   draggableElement() {
     Draggable.create('.drag-gif', {
       inertia: true,
@@ -45,29 +30,4 @@ export default class Gsap {
     });
     tl.to('.tagline', { duration: 2, text: 'Intégrateur web junior' });
   }
-  /*   responsiveAnimated() {
-    this.vw = window.innerWidth;
-    console.log(this.vw);
-    const softwares = document.querySelector('.grid__softwares');
-    const state = Flip.getState('.grid__softwares, .software');
-    if (this.vw <= 1000) {
-      softwares.classList.add('reorder');
-      Flip.from(state, {
-        // uses position: absolute during the flip to work around flexbox challenges
-        duration: 0.2,
-        stagger: 0.1,
-        ease: 'power1.inOut',
-        // you can use any other tweening properties here too, like onComplete, onUpdate, delay, etc.
-      });
-    } else {
-      softwares.classList.remove('reorder');
-      Flip.from(state, {
-        // uses position: absolute during the flip to work around flexbox challenges
-        duration: 0.2,
-        stagger: 0.1,
-        ease: 'power1.inOut',
-        // you can use any other tweening properties here too, like onComplete, onUpdate, delay, etc.
-      });
-    }
-  } */
 }
